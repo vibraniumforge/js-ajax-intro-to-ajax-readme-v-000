362aa56e3fe6aac703bd8a75284f23d247eef580
Greg Morgan <gregm9999@gmail.com>
2019-01-22 04:29:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
